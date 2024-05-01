0a7828c565a537174f92f7ac62b0ca134a9c41de
Volodymyr <116832648+Hospodaryk@users.noreply.github.com>
2024-05-01 14:11:58 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const btnDarkMode = document.querySelector(".dark-mode-btn");
+
+// 1. Перевірка темної теми на рівні системних налаштувань
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches ) {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+	document.body.classList.add("dark");
+}
+
+// 2. Перевірка темної теми в localStorage
+if (localStorage.getItem('darkMode') === 'dark') {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add("dark");
+} else if (localStorage.getItem("darkMode") === "light") {
+    btnDarkMode.classList.remove("dark-mode-btn--active");
+    document.body.classList.remove("dark");
+}
+
+// Якщо змінюються системні налаштування, змінюємо тему
+window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", (event) => {
+        const newColorScheme = event.matches ? "dark" : "light";
+
+        if (newColorScheme === "dark") {
+			btnDarkMode.classList.add("dark-mode-btn--active");
+			document.body.classList.add("dark");
+			localStorage.setItem("darkMode", "dark");
+		} else {
+			btnDarkMode.classList.remove("dark-mode-btn--active");
+			document.body.classList.remove("dark");
+			localStorage.setItem("darkMode", "light");
+		}
+    });
+
+// Увімкнення нічного режиму по кліку на кнопку
+btnDarkMode.onclick = function () {
+    btnDarkMode.classList.toggle("dark-mode-btn--active");
+    const isDark = document.body.classList.toggle("dark");
+
+    if (isDark) {
+        localStorage.setItem("darkMode", "dark");
+    } else {
+        localStorage.setItem("darkMode", "light");
+    }
+};
